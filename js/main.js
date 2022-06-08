effd922ebc34972cc0c8d2b057993db23f0ec23c
Romain Damiano <rdamianotravail@gmail.com>
2022-06-09 00:54:55 +0200

    Firebase database added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let database = firebase.database();
+const ref = firebase.database().ref(`creneaux`);
 let months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
 let time = new Date();
@@ -5,6 +7,9 @@ let month = time.getMonth();
 let year = time.getFullYear();
 
 document.addEventListener('DOMContentLoaded', function() {
+    // Modal info
+    let modal = document.getElementById("myModal");
+
     $.getJSON('config/config.json', function(data) {
 
         // DATE
@@ -16,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 year -= 1;
             }
             document.getElementById("date_js").innerText = months[month] + " " + year;
-            populate()
+            populate();
         });
         document.getElementById("next_js").addEventListener("click", function (){
             month++;
@@ -25,18 +30,20 @@ document.addEventListener('DOMContentLoaded', function() {
                 year++;
             }
             document.getElementById("date_js").innerText = months[month] + " " + year;
-            populate()
+            populate();
         });
         // END DATE
-        populate()
+        populate();
+        window.onresize = populate;
+
 
         function populate(){
-
             // MENU & CALENDARS
             let cal_menu_html = '';
             let cal_card_html = '';
             let i = 0;
-            let l;
+            let xxl;
+            let lg;
             let first_day_of_month = get_first_day_of_month();
             let ids = [];
             for (let category of data.categories){
@@ -57,8 +64,9 @@ document.addEventListener('DOMContentLoaded', function() {
                             let check_html = '';
                             for (let hour of place.hours){
                                 // TODO Link Checkbox with firebase
-                                check_html += `<td><input class="form-check-input" type="checkbox" id="${place.id}_${year}_${month + 1}_${day_date}_${hour}" value="" aria-label="..."></td>`;
-                                ids.push(`${place.id}_${year}_${month + 1}_${day_date}_${hour}`);
+                                let id = `${place.id}_${year}_${month + 1}_${day_date}_${hour}`;
+                                check_html += `<td><input class="form-check-input" type="checkbox" id="${id}" value="" aria-label="..."></td>`;
+                                ids.push(id);
                             }
                             days_html += `
                                     <tr>
@@ -69,14 +77,16 @@ document.addEventListener('DOMContentLoaded', function() {
                         }
                     if (place.hours.length < 3){
                         i++;
-                        l = 4;
+                        xxl = 4;
+                        lg = 6;
                     } else {
                         i += 1.5;
-                        l = 6;
+                        xxl = 6;
+                        lg = 12;
                     }
-                    if (i <= 1.5) { cal_card_html += `<div class="row">`; }
+                    if ( i <= 1.5 ) { cal_card_html += `<div class="row">`; }
                     cal_card_html += `
-                        <div id="${place.id}_cal" class="col-xl-${l} col-md-6 mb-4">
+                        <div id="${place.id}_cal" class="col-xxl-${xxl} col-lg-${lg} col-md-12 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-up">
@@ -108,7 +118,7 @@ document.addEventListener('DOMContentLoaded', function() {
                             </div>
                         </div>
                     `;
-                    if (i === 3){
+                    if (i === 3 || (window.innerWidth < 1400 && i ===2)){
                         cal_card_html += `</div>`;
                         i = 0;
                     }
@@ -116,13 +126,171 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             document.getElementById("calendars_menu_js").innerHTML = cal_menu_html;
             document.getElementById("cards_js").innerHTML = cal_card_html;
+
             for (let id of ids){
-                document.getElementById(id).addEventListener("change", function (event){
-                    // TODO Link with Firebase
-                })
+                ref.child(id).on('value', (snapshot) => {
+                    if (snapshot.val() != null) {
+                        if (snapshot.val().checked) {
+                            $(`#${id}`).prop('checked', true);
+                        } else {
+                            $(`#${id}`).prop('checked', false);
+                        }
+                    } else {
+                        $(`#${id}`).prop('checked', false);
+                    }
+                });
+
+                document.getElementById(id).addEventListener("click", function (event){
+                    event.preventDefault();
+                    let date_js = new Date(id.split('_')[1], parseInt(id.split('_')[2])-1, id.split('_')[3]);
+                    let day_js = date_js.getDay();
+                    if (day_js === 0) { day_js = 7 }
+                    let date = days[day_js - 1] + ' ' + date_js.getDate() + ' ' + months[date_js.getMonth()] + ' ' + date_js.getFullYear()
+                    let lieu = null;
+                    for (let category of data.categories) {
+                        for (let place of category.places) {
+                            if (place.id === id.split('_')[0]) { lieu = place.name + ' (' + place.address + ')'}
+                        }
+                    }
+                    let heure = id.split('_')[4] + 'h à ' + (parseInt(id.split('_')[4]) + 1).toString() + 'h'
+                    if ($(`#${id}`).prop("checked")) {
+                        document.getElementById("modal_js").innerHTML = `
+                            <div class="modal-header text-center">
+                                <h4 class="mt-2" style="margin-left: auto;margin-right: -10%;">RÉSERVATION</h4>
+                                <span class="close" onclick=''>&times;</span>
+                            </div>
+                            <div class="modal-body text-center">
+                                <p>Tu es sur le point de réserver le créneau TPL du<br><strong>${date}</strong><br>de <strong>${heure}</strong><br>à <strong>${lieu}</strong>.</p>
+                                <p>Confirmes-tu ?</p>
+                            </div>
+                            <div class="modal-footer justify-content-center">
+                                 <a href="javascript:;" id="cancel" class="btn btn-danger btn_modal">
+                                    <span class="icon">
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </a>
+                                 <a href="javascript:;" id="ok" class="btn btn-success btn_modal">
+                                    <span class="icon">
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                </a>
+                            </div>
+                        `;
+                        modal.style.display = "block";
+                        document.getElementsByClassName("close")[0].onclick = function () {
+                            modal.style.display = "none";
+                        }
+                        document.getElementById("cancel").onclick = function () {
+                            modal.style.display = "none";
+                        }
+                        document.getElementById("ok").onclick = function () {
+                            if (!$(`#${id}`).prop("checked")) {
+                                firebase.database().ref('creneaux/' + id).set({
+                                    checked: true
+                                });
+                                modal.style.display = "none";
+                                document.getElementById("modal_js").innerHTML = `
+                                    <div class="modal-header text-center">
+                                        <h4 class="mt-2" style="margin-left: auto;margin-right: -10%;">MERCI BEAUCOUP</h4>
+                                        <span class="close" onclick=''>&times;</span>
+                                    </div>
+                                    <div class="modal-body text-center">
+                                        <p>Merci d'avoir réserver ce créneau TPL.</p>
+                                        <p>Pour rappel, il s'agit du<br><strong>${date}</strong><br>de <strong>${heure}</strong><br>à <strong>${lieu}</strong>.</p>
+                                        <p>N'oublie pas de l'inscrire dans ton calendrier et de prévenir ton coéquipier.</p>
+                                        <p>Bonne prédication !</p>
+                                    </div>
+                                    <div class="modal-footer justify-content-center">
+                                         <a href="javascript:;" id="ok" class="btn btn-success btn_modal">
+                                            <span class="icon">
+                                                <i class="fas fa-check"></i>
+                                            </span>
+                                        </a>
+                                    </div>
+                                `;
+                                modal.style.display = "block";
+                                document.getElementsByClassName("close")[0].onclick = function () {
+                                    modal.style.display = "none";
+                                }
+                                document.getElementById("ok").onclick = function () {
+                                    modal.style.display = "none";
+                                }
+                            } else {
+                                modal.style.display = "none";
+                                document.getElementById("modal_js").innerHTML = `
+                                    <div class="modal-header text-center">
+                                        <h4 class="mt-2" style="margin-left: auto;margin-right: -10%;">OH NON !</h4>
+                                        <span class="close" onclick=''>&times;</span>
+                                    </div>
+                                    <div class="modal-body text-center">
+                                        <p>Désolé, tu n'as pas été assez rapide.</p>
+                                        <p>Quelqu'un d'autre vient tout juste de réserver ce créneau pendant que tu réfléchissais.</p>
+                                        <p>Ce n'est pas grave, peut-être qu'à un autre endroit ou à un autre horaire il reste une disponibilité.</p>
+                                    </div>
+                                    <div class="modal-footer justify-content-center">
+                                         <a href="javascript:;" id="ok" class="btn btn-success btn_modal">
+                                            <span class="icon">
+                                                <i class="fas fa-check"></i>
+                                            </span>
+                                        </a>
+                                    </div>
+                                `;
+                                modal.style.display = "block";
+                                document.getElementsByClassName("close")[0].onclick = function () {
+                                    modal.style.display = "none";
+                                }
+                                document.getElementById("ok").onclick = function () {
+                                    modal.style.display = "none";
+                                }
+                            }
+                        }
+                    } else {
+                        document.getElementById("modal_js").innerHTML = `
+                            <div class="modal-header text-center">
+                                <h4 class="mt-2" style="margin-left: auto;margin-right: -10%;">ATTENTION</h4>
+                                <span class="close" onclick=''>&times;</span>
+                            </div>
+                            <div class="modal-body text-center">
+                                <p>Tu es sur le point supprimer cette réservation pour le créneau TPL du<br><strong>${date}</strong><br>de <strong>${heure}</strong><br>à <strong>${lieu}</strong>.</p>
+                                <p>Veux-tu le supprimer ?</p>
+                            </div>
+                            <div class="modal-footer justify-content-center">
+                                 <a href="javascript:;" id="cancel" class="btn btn-danger btn_modal">
+                                    <span class="icon">
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </a>
+                                 <a href="javascript:;" id="ok" class="btn btn-success btn_modal">
+                                    <span class="icon">
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                </a>
+                            </div>
+                        `;
+                        modal.style.display = "block";
+                        document.getElementsByClassName("close")[0].onclick = function () {
+                            modal.style.display = "none";
+                        }
+                        document.getElementById("cancel").onclick = function () {
+                            modal.style.display = "none";
+                        }
+                        document.getElementById("ok").onclick = function () {
+                            firebase.database().ref('creneaux/' + id).set({
+                                checked: false
+                            });
+                            modal.style.display = "none";
+                        }
+                    }
+                });
             }
             // END MENU & CALENDARS
 
+        }
+
+        window.onclick = function(event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
         }
 
     });
